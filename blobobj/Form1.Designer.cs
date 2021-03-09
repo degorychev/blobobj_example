2d76a78d9e185a0880c052f2d089417dcdaf39ea
315-01 <315-01@iatu.local>
2021-03-09 11:54:26 +0400

    Добавьте файлы проекта.

---
+++ blobobj/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace blobobj
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(178, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(610, 426);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(160, 394);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.ListBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.Location = new System.Drawing.Point(12, 415);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(160, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ blobobj/Form1.cs
@@ -0,0 +1,172 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace blobobj
+{
+    public partial class Form1 : Form
+    {
+        dbworker dbw = new dbworker("localhost", "root", "", "test");
+        public Form1()
+        {
+            InitializeComponent();
+            loadImages();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.ShowDialog();
+            Image img = Image.FromFile(ofd.FileName);
+            dbw.insertImg(img, ofd.FileName);
+        }
+
+        private void loadImages()
+        {
+            var images = dbw.getimages();
+            listBox1.DataSource = images;
+            listBox1.ValueMember = "id";
+            listBox1.DisplayMember = "name";
+        }
+
+        private void ListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                pictureBox1.Image = dbw.getImg(Convert.ToInt32(listBox1.SelectedValue));
+            }
+            catch(Exception ex)
+            {
+            }
+        }
+    }
+    class dbworker
+    {
+        MySqlConnection Connection;
+        MySqlConnectionStringBuilder mySqlConnectionStringBuilder = new MySqlConnectionStringBuilder();
+        public dbworker(string server, string user, string pass, string database)
+        {
+            mySqlConnectionStringBuilder.Server = server;
+            mySqlConnectionStringBuilder.UserID = user;
+            mySqlConnectionStringBuilder.Password = pass;
+            mySqlConnectionStringBuilder.Port = 3306;
+            mySqlConnectionStringBuilder.Database = database;
+            mySqlConnectionStringBuilder.CharacterSet = "utf8";
+            Connection = new MySqlConnection(mySqlConnectionStringBuilder.ConnectionString);
+        }
+
+        public void insertImg(Image img, string name)
+        {
+            var bytes = imageToByte(img);
+            var command = Connection.CreateCommand();
+            command.CommandText = "INSERT INTO images(`name`, `img`, `ImageSize`) VALUES (@name, @image, @size);";
+
+            var paramUserImage = new MySqlParameter("@image", MySqlDbType.Blob, bytes.Length);
+            paramUserImage.Value = bytes;
+            command.Parameters.Add(paramUserImage);
+            command.Parameters.Add("@name", MySqlDbType.VarChar).Value = name;
+            command.Parameters.Add("@size", MySqlDbType.Int32).Value = bytes.Length;
+            try
+            {
+                Connection.Open();
+                command.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+            finally
+            {
+                Connection.Close();
+            }
+        }
+        public DataTable getimages()
+        {
+            var cmd = Connection.CreateCommand();
+            cmd.CommandText = "SELECT `id`, `name` FROM images";
+            DataTable dt = new DataTable();
+            try
+            {
+                Connection.Open();
+                dt.Load(cmd.ExecuteReader());
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+            finally
+            {
+                Connection.Close();
+            }
+            return dt;
+        }
+
+        public Image getImg(int id)
+        {
+            byte[] rawData;
+            UInt32 FileSize;
+            Image outImage;
+
+
+            var command = Connection.CreateCommand();
+            command.CommandText = "SELECT `img`, `ImageSize` FROM images WHERE `id`=@id";
+            command.Parameters.Add("@id", MySqlDbType.Int32).Value = id;
+            try
+            {
+                Connection.Open();
+                var myData = command.ExecuteReader();
+                if (myData.Read())
+                {
+                    FileSize = myData.GetUInt32(myData.GetOrdinal("ImageSize"));
+                    rawData = new byte[FileSize];
+
+                    myData.GetBytes(myData.GetOrdinal("img"), 0, rawData, 0, (Int32)FileSize);
+                    outImage = Image.FromStream(new MemoryStream(rawData));
+
+
+                    myData.Close();
+                    myData.Dispose();
+
+                    command.Dispose();
+
+                    return outImage;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                Connection.Close();
+            }
+            return null;
+        }
+
+        private byte[] imageToByte(Image img)
+        {
+            using (var ms = new MemoryStream())
+            {
+                img.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
+                return ms.ToArray();
+            }
+        }
+
+        private Image ImageFromByte(byte[] bytes)
+        {
+            using (var ms = new MemoryStream(bytes))
+            {
+                return Image.FromStream(ms);
+            }
+        }
+    }
+}
